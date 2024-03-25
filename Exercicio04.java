bf929a9d8a4dc0c9e6337c84a4eb99535d68c421
yohaa22 <145264388+yohaa22@users.noreply.github.com>
2024-03-25 19:21:51 -0300

    Exercicio04

---
+++ Exercicio04.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Exercicio04 {
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+     System.out.print("Digite o primeiro número: ");
+        int num = scanner.nextInt();
+
+    System.out.print("Digite o segundo número : ");
+            int num2 = scanner.nextInt();
+
+
+System.out.println("o resultado da soma é: " + (num + num2));
+System.out.println("o resultado da subtração é: " + (num - num2));
+System.out.println("o resultado da multiplicação é: " + (num * num2));
+System.out.println("o resultado da divisão é: " + (num / num2));
+
+
+
+
+
+
+
+}
+}
--- Programa.java
+++ Programa.java
@@ -2,7 +2,9 @@ public class Programa {
     public static void main(String[] args) {
        // Exercicio01.main(args);
         
-        Exercicio02.main(args);
+        //Exercicio02.main(args);
+
+        Exercicio04.main(args);
        
     }
 }
