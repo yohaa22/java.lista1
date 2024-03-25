4594742c227607a8dbe3fe46eb966c9e9634a17b
yohaa22 <145264388+yohaa22@users.noreply.github.com>
2024-03-25 19:16:40 -0300

    Exercicio02

---
+++ Exercicio02.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Exercicio02 {
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+     System.out.println("Digite um número: ");
+        int num = scanner.nextInt();
+
+    System.out.println("Digite um número para somar: ");
+            int num2 = scanner.nextInt();
+
+
+System.out.println("o resultado é: " + (num + num2));
+
+
+
+
+
+
+
+}   
+}
+
--- Programa.java
+++ Programa.java
@@ -1,6 +1,9 @@
 public class Programa {
     public static void main(String[] args) {
-        Exercicio01.main(args);
+       // Exercicio01.main(args);
+        
+        Exercicio02.main(args);
        
     }
 }
+
